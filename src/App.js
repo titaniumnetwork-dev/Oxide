6f2a9f1763662c1f4174ba80c1847aa7324ab243
QuiteAFancyEmerald <46467239+QuiteAFancyEmerald@users.noreply.github.com>
2022-06-24 17:33:22 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import Nav from "./components/layouts/Nav";
+import Body from "./components/layouts/Body";
+import ErrorBody from "./components/layouts/Body";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Nav />
+      <Body />
+    </React.Fragment>
   );
 }
 
-export default App;
+function Error() {
+  return (
+    <React.Fragment>
+      <Nav />
+      <ErrorBody />
+    </React.Fragment>
+  );
+}
+
+export { App, Error };
