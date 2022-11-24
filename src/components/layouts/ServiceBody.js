20f9fcc08d50f17a4ae973f739a5025dfbcea4b5
QuiteAFancyEmerald <46467239+QuiteAFancyEmerald@users.noreply.github.com>
2022-11-24 17:14:06 -0600

    Added Nebula

--- src/components/layouts/ServiceBody.js
+++ src/components/layouts/ServiceBody.js
@@ -20,6 +20,7 @@ function ServicesBody() {
             <li><a href="https://holyubofficial.net">Holy Unblocker</a><div><a href="https://holyubofficial.net">❐</a></div></li>
             <li><a href="https://incog.dev">Incognito</a><div><a href="https://incog.dev">❐</a></div></li>
             <li><a href="https://radon.games">Radon Games</a><div><a href="https://radon.games">❐</a></div></li>
+            <li><a href="https://nebulaproxy.io">Nebula Proxy</a><div><a href="https://nebulaproxy.io">❐</a></div></li>
             <li><a href="https://hypertabs.cc">Hypertabs</a><div><a href="https://hypertabs.cc">❐</a></div></li>
             <li><a href="https://illusive.app/">Illusive</a><div><a href="https://illusive.app/">❐</a></div></li>
             <li><a href="https://terbium.ga/">Terbium UX</a><div><a href="https://terbium.ga/">❐</a></div></li>
@@ -28,10 +29,8 @@ function ServicesBody() {
             <br></br><h3 id="web-proxy-services">Community Proxy Services</h3>
             <br></br>
             <ul className="service-lists">
-            <li>Nebula</li>
             <li>Astral (Lucid)</li>
             <li>SealCentral</li>
-            <li>Radon Games</li>
             <li>Abyss</li>
             </ul>
           </div>
