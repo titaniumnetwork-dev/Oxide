06f769ef5241217f8740320f490c88425fa41c45
David Reed <reedswork@proton.me>
2023-03-22 14:27:33 -0400

    unused

--- public/main.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    const nav = document.querySelector('.navbar');
-    const input = document.querySelector('.responsive');
-
-    input.addEventListener("click", () => {
-        nav.classList.toggle('nav-enabled');
-    });
-})();
